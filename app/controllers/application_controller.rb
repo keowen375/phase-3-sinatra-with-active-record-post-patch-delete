53aeabc2a93fc13865ff37873d275b9a5d5bf152
keowen375@gmail.com <keowen375@gmail.com>
2023-06-09 18:15:31 +0300

    Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,48 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-
-  get '/games' do
-    games = Game.all.order(:title).limit(10)
-    games.to_json
+  set default_content_type: "application/json"
+  
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
   end
 
-  get '/games/:id' do
-    game = Game.find(params[:id])
-
-    game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+  get '/baked_goods/by_price' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
   end
+
+  get '/baked_goods/most_expensive' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(name: params[:name])
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
+
 
 end
